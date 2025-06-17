2f073ed83b1c108e1b4a8178bb340d0c22907fd6
Maike Miller Figueiredo e Silva <maikemiler@hotmail.com>
2025-06-17 18:13:21 -0300

    Adicionar arquivos de projeto.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,24 @@
+﻿using PickRandomCards;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.Write("Insira o numero de cartas que você quer pegar: ");
+        string line = Console.ReadLine();
+
+
+        if (int.TryParse(line, out int numberOfCards))
+        {
+            foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+            {
+                Console.WriteLine(card);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Por favor, insira um numero valido.");
+        }
+    }
+}
